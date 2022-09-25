2b6d36e14473297fdcc5eeab03159371cd5eaf1c
Pandey Kavyansh <kavpandey@deloitte.com>
2022-09-26 00:40:01 +0530

    Added Error component with basic styling

---
+++ src/components/Error.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Error = () => {
+    return (
+        <div style={{fontSize : "3em", marginTop: "1em"}}>
+            404, Content not found.
+        </div>
+    )
+}
+
+export default Error;
